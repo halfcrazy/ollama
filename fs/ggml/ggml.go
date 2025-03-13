ee62645839b60465e6164f0f48c057491a5a1f79
Michael Yang <mxyng@pm.me>
2025-03-13 16:53:22 -0700

    count non-repeating vision layers

--- fs/ggml/ggml.go
+++ fs/ggml/ggml.go
@@ -588,7 +588,7 @@ func (llm GGML) VisionGraphSize() (weights, graphSize uint64) {
 	}
 
 	for name, layer := range llm.Tensors().GroupLayers() {
-		if strings.HasPrefix(name, "v.") {
+		if name == "v" || strings.HasPrefix(name, "v.") {
 			for _, tensor := range layer {
 				weights += tensor.Size()
 			}
